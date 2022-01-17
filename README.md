663dcaf220b9b438f0de65643f6bf4a5988fd6d8
Aaron DeVera <aaron@backchannel.re>
2022-01-17 02:47:27 -0800

    project fork init

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-# Picasso based canvas fingerprinting
-Implementation of a canvas fingerprinting algorithm inspired by the [Picasso paper](https://ai.google/research/pubs/pub45581) written by Elie Bursztein.
+# dali
+Dali is a Picasso-like device attestation payload
 
-An online demo is available on [my blog](https://antoinevastel.com/browser%20fingerprinting/2019/03/21/picasso-canvas-fingerprinting.html).
+## Use in-browser
+Use the minified version in `dist`:
+```
+[7.8K]  dali.min.js 13f558356a2c193e75823556fe9ceb6166dd91abc671a390ff6aead392892590
+```
+You can call it like so, with all the parameters in the exact order:
+```
+<script>
+    dali([
+        15, // repetitions
+        8547498279832982569844, // seed
+        500, // size
+        2001000001, // offset
+        15000, // multiplier
+        20, // fontsize
+        50 // maxblur
+    ]).then(response => {
+        const imgEle = document.createElement("img");
+        imgEle.src = response.url;
+        document.getElementById("dali-hash").innerHTML = response.hash;
+        document.getElementById("dali-canvas").appendChild(imgEle);
+    });
+</script>
+```
+When called, the `dali` function will generate a [Promise Response](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise) of:
+```
+{"url":"<url_string>","hash":"<sha256_hash_string>"}
+```
+
+## Copyright
+`dali.js` Includes the elements from the following open source projects:
+- [picasso-like-canvas-fingerprinting](https://github.com/antoinevastel/picasso-like-canvas-fingerprinting) (MIT License Copyright (c) 2019 antoine vastel)
+
+
+## References
+- [Google Research, Picasso: Lightweight Device Class Fingerprinting for Web Clients](https://research.google/pubs/pub45581/)
+- [@antoinevastel, Picasso based canvas fingerprinting](https://github.com/antoinevastel/picasso-like-canvas-fingerprinting)
+- [FingerprintJS, How Does Canvas Fingerprinting Work?](https://fingerprintjs.com/blog/canvas-fingerprinting/)
+- [AdTechMadness, Overview of Google’s Picasso](https://adtechmadness.wordpress.com/2019/03/19/overview-of-googles-picasso/)
---
+++ dali.js
@@ -0,0 +1,174 @@
+async function dali() {
+    const repetitions = params[0];
+    const seed = params[1];
+    
+    const width = params[2];
+    const height = params[2];
+
+    const offset = params[3];
+    const multiplier = params[4];
+    const fontsize = params[5];
+    const maxblur = params[6];
+
+    class Prng {
+        constructor(seed) {
+            this.currentNumber = seed % offset;
+            if (this.currentNumber <= 0) {
+                this.currentNumber += offset
+            }
+        }
+        getNext() {
+            this.currentNumber = multiplier * this.currentNumber % offset;
+            return this.currentNumber;
+        }
+    }
+
+    async function digestMessage(message) {
+        const msgUint8 = new TextEncoder().encode(message);
+        const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        return hashHex;
+     }
+     
+
+    function adaptRandomNumberToContext(randomNumber, maxBound, floatAllowed) {
+        randomNumber = (randomNumber - 1) / offset;
+        if (floatAllowed) {
+            return randomNumber * maxBound;
+        }
+
+        return Math.floor(randomNumber * maxBound);
+    }
+
+    function addRandomCanvasGradient(prng, context) {
+        const canvasGradient = context.createRadialGradient(
+            adaptRandomNumberToContext(prng.getNext(), width),
+            adaptRandomNumberToContext(prng.getNext(), height),
+            adaptRandomNumberToContext(prng.getNext(), width),
+            adaptRandomNumberToContext(prng.getNext(), width),
+            adaptRandomNumberToContext(prng.getNext(), height),
+            adaptRandomNumberToContext(prng.getNext(), width)
+        );
+        canvasGradient.addColorStop(0, colors[adaptRandomNumberToContext(prng.getNext(), colors.length)]);
+        canvasGradient.addColorStop(1, colors[adaptRandomNumberToContext(prng.getNext(), colors.length)]);
+        context.fillStyle = canvasGradient
+    }
+
+    function generateRandomWord(prng, wordLength) {
+        const minAscii = 65;
+        const maxAscii = 126;
+        const wordGenerated = [];
+        for (let i = 0; i < wordLength; i++) {
+            const asciiCode = minAscii + (prng.getNext() % (maxAscii - minAscii));
+            wordGenerated.push(String.fromCharCode(asciiCode));
+        }
+
+        return wordGenerated.join('');
+    }
+
+    if (!window.CanvasRenderingContext2D) {
+        return 'unknown';
+    }
+
+    const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+        '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+        '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+        '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+        '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+        '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+        '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+        '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+    const primitives = [
+        function arc (prng, context) {
+            context.beginPath();
+            context.arc(
+                adaptRandomNumberToContext(prng.getNext(), width),
+                adaptRandomNumberToContext(prng.getNext(), height),
+                adaptRandomNumberToContext(prng.getNext(), Math.min(width, height)),
+                adaptRandomNumberToContext(prng.getNext(), 2 * Math.PI, true),
+                adaptRandomNumberToContext(prng.getNext(), 2 * Math.PI, true)
+            );
+            context.stroke()
+        },
+        function text (prng, context) {
+            const wordLength = Math.max(1, adaptRandomNumberToContext(prng.getNext(), 5));
+            const textToStroke = generateRandomWord(prng, wordLength);
+            context.font = `${fontsize}px aafakefontaa`;
+
+            context.strokeText(
+              textToStroke,
+              adaptRandomNumberToContext(prng.getNext(), width),
+              adaptRandomNumberToContext(prng.getNext(), height),
+              adaptRandomNumberToContext(prng.getNext(), width)
+            )
+        },
+        function bezierCurve (prng, context) {
+            context.beginPath();
+            context.moveTo(
+                adaptRandomNumberToContext(prng.getNext(), width),
+                adaptRandomNumberToContext(prng.getNext(), height)
+            );
+            context.bezierCurveTo(
+                adaptRandomNumberToContext(prng.getNext(), width),
+                adaptRandomNumberToContext(prng.getNext(), height),
+                adaptRandomNumberToContext(prng.getNext(), width),
+                adaptRandomNumberToContext(prng.getNext(), height),
+                adaptRandomNumberToContext(prng.getNext(), width),
+                adaptRandomNumberToContext(prng.getNext(), height)
+            );
+            context.stroke()
+        },
+        function quadraticCurve(prng, context) {
+            context.beginPath();
+            context.moveTo(
+                adaptRandomNumberToContext(prng.getNext(), width),
+                adaptRandomNumberToContext(prng.getNext(), height)
+            );
+            context.quadraticCurveTo(
+                adaptRandomNumberToContext(prng.getNext(), width),
+                adaptRandomNumberToContext(prng.getNext(), height),
+                adaptRandomNumberToContext(prng.getNext(), width),
+                adaptRandomNumberToContext(prng.getNext(), height)
+            );
+            context.stroke()
+        },
+        function ellipse(prng, context) {
+            context.beginPath();
+            context.ellipse(
+                adaptRandomNumberToContext(prng.getNext(), width),
+                adaptRandomNumberToContext(prng.getNext(), height),
+                adaptRandomNumberToContext(prng.getNext(), Math.floor(width/2)),
+                adaptRandomNumberToContext(prng.getNext(), Math.floor(height/2)),
+                adaptRandomNumberToContext(prng.getNext(), 2 * Math.PI, true),
+                adaptRandomNumberToContext(prng.getNext(), 2 * Math.PI, true),
+                adaptRandomNumberToContext(prng.getNext(), 2 * Math.PI, true)
+            );
+
+            context.stroke()
+        }
+    ];
+
+    try {
+        const prng = new Prng(seed);
+        const canvasElt = document.createElement("canvas");
+        canvasElt.width = width;
+        canvasElt.height = height;
+        const context = canvasElt.getContext("2d");
+        for (let i = 0; i < repetitions; i++) {
+            addRandomCanvasGradient(prng, context);
+            context.shadowBlur = adaptRandomNumberToContext(prng.getNext(), maxblur);
+            context.shadowColor = colors[adaptRandomNumberToContext(prng.getNext(), colors.length)];
+            const randomPrimitive = primitives[adaptRandomNumberToContext(prng.getNext(), primitives.length)];
+            randomPrimitive(prng, context);
+            context.fill()
+            
+        }
+        const url = canvasElt.toDataURL();
+        const hash = String(await digestMessage(url))
+        return {url,hash};
+    } catch (e) {console.log(e)}
+}
